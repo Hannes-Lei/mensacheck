8960e0787708b94a332edf6b2b5aa05ac26cd41b
Hannes-Lei <hannesjakob25@gmail.com>
2026-06-30 18:55:21 +0200

    Delete src/src/main.jsx

--- src/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import MensaCheck from "./MensaCheck.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <MensaCheck />
-  </React.StrictMode>
-);

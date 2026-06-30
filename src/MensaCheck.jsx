3fb5dfd3d8e33a4db4571170d2e5a89c7773ba05
Hannes-Lei <hannesjakob25@gmail.com>
2026-06-30 18:54:25 +0200

    Add MensaCheck component to the application

---
+++ src/MensaCheck.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MensaCheck from "./MensaCheck.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <MensaCheck />
+  </React.StrictMode>
+);
